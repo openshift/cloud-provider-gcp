38b00c99260cf706e27a4e57dd33a1b5e9240847
Matthew Booth <mbooth@redhat.com>
2026-06-30 17:37:27 +0100

    UPSTREAM: <carry>: Fix node.kubernetes.io/exclude-from-external-load-balancers on masters
    We previously used allHaveNodePrefix() to ensure that if a master
    instance group contained the bootstrap machine this would not cause it
    to be excluded from re-use. This worked because the bootstrap machine's
    name has the same prefix as all other machines in the cluster.
    
    However, this logic unconditionally always included the master instance
    group. If the masters are labelled with
    node.kubernetes.io/exclude-from-external-load-balancers the CCM
    framework code will have filtered the Nodes before passing them to
    provider-GCP. But this logic meant we added them anyway, sending
    unintended traffic to the control plane machines.

--- providers/gce/gce_loadbalancer_internal_openshift.go
+++ providers/gce/gce_loadbalancer_internal_openshift.go
@@ -17,6 +17,7 @@ limitations under the License.
 package gce
 
 import (
+	"fmt"
 	"slices"
 	"strings"
 
@@ -144,37 +145,24 @@ func (g *Cloud) evaluateExternalInstanceGroup(ig *compute.InstanceGroup, zone st
 	// Extract instance names from the group
 	instanceNames = extractInstanceNamesFromGroup(instances)
 
-	// If all instances in this external instance group are also in our zone's node list,
-	// or they all have the node instance prefix, we can reuse this instance group instead
-	// of creating our own internal instance group
-	hasAll := gceHostNamesInZone.HasAll(instanceNames.UnsortedList()...)
-	allHavePrefix := g.allHaveNodePrefix(instanceNames.UnsortedList())
-	shouldReuse = hasAll || allHavePrefix
-	klog.V(2).Infof("evaluateExternalInstanceGroup(%v): shouldReuse=%v (hasAll=%v, allHavePrefix=%v), instances=%v", ig.Name, shouldReuse, hasAll, allHavePrefix, instanceNames.UnsortedList())
+	// During bootstrap the bootstrap machine is placed in one of the master
+	// instance groups. However, the bootstrap machine does not have an
+	// associated Node. This will prevent us from considering this instance
+	// group for reuse because the instance group contains an instance which is
+	// not in the service's Node list. Consequently we will attempt to add the
+	// masters to a second instance group, which will fail with
+	// INSTANCE_IN_MULTIPLE_LOAD_BALANCED_IGS.
+	//
+	// To avoid this we explicitly exclude the bootstrap machine from
+	// consideration if it is present.
+	bootstrapInstanceName := fmt.Sprintf("%s-bootstrap", g.nodeInstancePrefix)
+	instanceNames.Delete(bootstrapInstanceName)
+
+	// If all instances in this external instance group are also in our zone's node list
+	shouldReuse = gceHostNamesInZone.HasAll(instanceNames.UnsortedList()...) && instanceNames.Len() > 0
+	klog.V(2).Infof("evaluateExternalInstanceGroup(%v): shouldReuse=%v, instances=%v", ig.Name, shouldReuse, instanceNames.UnsortedList())
 
 	return shouldReuse, instanceNames, nil
-}
-
-// allHaveNodePrefix checks if all instances have the cluster's node instance prefix.
-//
-// DO NOT REMOVE. This is load-bearing for all OCP GCP clusters.
-//
-// The OpenShift installer creates per-zone master instance groups (e.g.
-// <infra>-master-<zone>). During CAPG installs (OCPBUGS-35256), the bootstrap
-// node is placed in the same master IG as a control plane node. The bootstrap
-// node is not a k8s node, so HasAll rejects the master IG. Without this prefix
-// fallback the CCM then tries to add the master to its own k8s-ig, which fails
-// with INSTANCE_IN_MULTIPLE_LOAD_BALANCED_IGS (master is already in the
-// installer's IG). It then tries to add the worker to that same k8s-ig
-// alongside the master, which fails with wrongSubnetwork (masters and workers
-// are on different subnets).
-func (g *Cloud) allHaveNodePrefix(instances []string) bool {
-	for _, instance := range instances {
-		if !strings.HasPrefix(instance, g.nodeInstancePrefix) {
-			return false
-		}
-	}
-	return true
 }
 
 // candidateExternalInstanceGroups returns instance groups with the external instance groups prefix, if defined.
